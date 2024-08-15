d0272b7af4f5614470c78ccff40ba0c04f0f4786
smimdla0205 <sangmin0205@gmail.com>
2024-08-15 18:23:54 +0900

    feat: badge 컴포넌트 완성

---
+++ src/components/common/badge/index.tsx
@@ -0,0 +1,21 @@
+import classNames from "classnames/bind";
+
+import styles from "./badge.module.scss";
+
+interface BadgeProps {
+  text: string;
+  color: "gray" | "green" | "red" | "gray-deep" | "green-deep";
+  Rounded?: "small" | "large";
+  font?: string;
+  onClick?: () => void;
+}
+
+const cx = classNames.bind(styles);
+
+export const Badge = ({ text, color, Rounded = "small", font = "label4-regular", onClick }: BadgeProps) => {
+  return (
+    <div className={cx("badge", color, font, Rounded === "small" ? "round-small" : "round-large")} onClick={onClick}>
+      {text}
+    </div>
+  );
+};
