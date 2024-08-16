26602173991a9a86c75e4ca0487abac61f7be841
bona1122 <vnclfjqm12@gmail.com>
2024-08-16 18:00:18 +0900

    test: menubox test

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,7 +3,9 @@
 import { Button } from "@headlessui/react";
 import classNames from "classnames/bind";
 
+import Pencil from "@/assets/icon/icon-pencil-square.svg";
 import HomeIcon from "@/assets/tabBar/icon-home.svg";
+import { Menubox } from "@/components/Menubox";
 import { useStore } from "@/stores/test";
 
 import style from "./page.module.scss";
@@ -25,6 +27,23 @@ export default function Home() {
       </button>
       <Button className={cn("button")}>Save changes</Button>
       <HomeIcon />
+
+      <Menubox
+        triggerButton={<Button>Open</Button>}
+        items={[
+          { content: "Item 1" },
+          { content: "Item 2" },
+          { content: "Item 3" },
+        ]}
+      />
+      <Menubox
+        triggerButton={<Button>Open</Button>}
+        items={[
+          { content: "Item 1", icon: <Pencil /> },
+          { content: "Item 2", icon: <Pencil /> },
+          { content: "Item 3", icon: <Pencil /> },
+        ]}
+      />
     </div>
   );
 }
