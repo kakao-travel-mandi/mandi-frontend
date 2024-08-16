ffec71f290508644d4a96b214037dfc5e78c41f9
smimdla0205 <sangmin0205@gmail.com>
2024-08-16 18:25:59 +0900

    feat: Rounded=>rounded로 변경

--- src/components/common/badge/index.tsx
+++ src/components/common/badge/index.tsx
@@ -5,16 +5,16 @@ import styles from "./badge.module.scss";
 interface BadgeProps {
   text: string;
   color: "gray" | "green" | "red" | "green-deep";
-  Rounded?: "small" | "large";
+  rounded?: "small" | "large";
   font?: string;
   onClick?: () => void;
 }
 
 const cx = classNames.bind(styles);
 
-export const Badge = ({ text, color, Rounded = "small", font = "label4-regular", onClick }: BadgeProps) => {
+export const Badge = ({ text, color, rounded = "small", font = "label4-regular", onClick }: BadgeProps) => {
   return (
-    <div className={cx("badge", color, font, Rounded === "small" ? "round-small" : "round-large")} onClick={onClick}>
+    <div className={cx("badge", color, font, rounded === "small" ? "round-small" : "round-large")} onClick={onClick}>
       {text}
     </div>
   );
