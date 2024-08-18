08a72ad657f8ebda3bc093d10202e5123e81c6f6
huisso97 <chosohi@naver.com>
2024-08-18 22:45:30 +0900

    fix: viewport 분리

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,14 +8,14 @@ const Pretendard = localFont({
   src: '../styles/fonts/PretendardVariable.woff2',
 });
 
-export const viewport = {
-  themeColor: '#ffffff',
-};
-
 export const metadata: Metadata = {
   title: 'Mandi',
   description: 'Mandi',
   manifest: '/manifest.json',
+};
+
+export const viewport = {
+  themeColor: '#ffffff',
   viewport:
     'minimum-scale=1, initial-scale=1, maximum-scale=1, width=device-width, shrink-to-fit=no, viewport-fit=cover user-scalable=no',
 };
