9c85dbe94530331090fe5a9535a354ae36d34abb
huisso97 <chosohi@naver.com>
2024-09-03 19:26:43 +0900

    fix: layout 탑, 바텀 내브 기준 스타일 조정

--- src/components/common/top-navbar/index.tsx
+++ src/components/common/top-navbar/index.tsx
@@ -1,13 +1,13 @@
 'use client';
 
 import classNames from 'classnames/bind';
-import {useRouter} from 'next/navigation';
+import { useRouter } from 'next/navigation';
 
 import Back from '@/assets/icon/icon-arrow-left-small-mono.svg';
 import Mandi from '@/assets/logo/Mandi.svg';
 
-import {IconActionButtonProps, IconButton} from './IconActionButton';
-import {TextActionButtonProps, TextButton} from './TextActionButton';
+import { IconActionButtonProps, IconButton } from './IconActionButton';
+import { TextActionButtonProps, TextButton } from './TextActionButton';
 import styles from './index.module.scss';
 
 const BLOCK = 'top-navbar';
@@ -19,11 +19,24 @@ interface TopNavBarProps {
   title?: string;
   actions?: (TextActionButtonProps | IconActionButtonProps)[];
   back?: boolean;
+  onBack?: () => void;
 }
 
-export const TopNavBar = ({logo, title, actions, back}: TopNavBarProps) => {
+export const TopNavBar = ({
+  logo,
+  title,
+  actions,
+  back,
+  onBack,
+}: TopNavBarProps) => {
   const router = useRouter();
-  const handleBack = () => router.back();
+  const handleBack = () => {
+    if (onBack) {
+      onBack();
+    } else {
+      router.back();
+    }
+  };
 
   return (
     <div className={cx(BLOCK)}>
@@ -38,10 +51,10 @@ export const TopNavBar = ({logo, title, actions, back}: TopNavBarProps) => {
           actions.map((Action, index) => {
             // 액션타입이 TextButtonProps면 TextButton 컴포넌트로 렌더링
             if ('text' in Action) {
-              const {text, onClick} = Action as TextActionButtonProps;
+              const { text, onClick } = Action as TextActionButtonProps;
               return <TextButton text={text} onClick={onClick} key={index} />;
             } else {
-              const {icon, onClick} = Action as IconActionButtonProps;
+              const { icon, onClick } = Action as IconActionButtonProps;
               return <IconButton icon={icon} onClick={onClick} key={index} />;
             }
           })}
