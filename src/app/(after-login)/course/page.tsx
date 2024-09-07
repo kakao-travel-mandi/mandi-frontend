5dfdd02a497e1fb3a2c2be71aa1328c97330d0f8
bona1122 <vnclfjqm12@gmail.com>
2024-09-08 03:41:45 +0900

    feat: add draggable bottom sheet component

--- src/app/(after-login)/course/page.tsx
+++ src/app/(after-login)/course/page.tsx
@@ -1,9 +1,46 @@
+'use client';
+import { useState } from 'react';
+
+import BottomSheet from '@/components/common/bottomsheet';
+import DraggableBottomSheet from '@/components/common/draggable-bottomsheet';
 import Layout from '@/components/layout';
 
 const Course = () => {
+  const [isOpen, setIsOpen] = useState(true);
   return (
     <Layout hasTopNav={true} hasTabBar={false} back={true} title='Course'>
       <div>Course</div>
+      <DraggableBottomSheet isOpen={true} onClose={() => setIsOpen(false)}>
+        <div style={{
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          justifyContent: 'center',
+          gap: '20px',
+        }}>
+          <div
+            style={{
+              width: '500px',
+              height: '500px',
+              backgroundColor: 'green',
+            }}
+          />
+          <div
+            style={{
+              width: '500px',
+              height: '500px',
+              backgroundColor: 'green',
+            }}
+          />
+          <div
+            style={{
+              width: '500px',
+              height: '500px',
+              backgroundColor: 'green',
+            }}
+          />
+        </div>
+      </DraggableBottomSheet>
     </Layout>
   );
 };
