7f3e492565ebcb25a17a2800045de78808b9759b
bona1122 <vnclfjqm12@gmail.com>
2024-09-27 23:01:06 +0900

    fix: fix bottom-area-button in myInfo page

--- src/app/(after-login)/my-info/delete-account/page.tsx
+++ src/app/(after-login)/my-info/delete-account/page.tsx
@@ -34,7 +34,6 @@ export default function Page() {
   const [deleteDialogOpen, setDeleteDialogOpen] = useState(false);
   const handleDialogClose = () => setDeleteDialogOpen(false);
 
-  // my-info로 이동
   const handleContinueClick = () => router.push('/my-info');
   const handleDeleteClick = () => setDeleteDialogOpen(true);
 
@@ -48,41 +47,39 @@ export default function Page() {
       back={true}
       hasTabBar={false}
     >
-      <SizedBox height='2.75rem' />
-      <div className={cx(BLOCK)}>
-        <h2 className={cx(`${BLOCK}__title`)}>
-          Are you sure you want to
-          <br />
-          delete your Mandi account?
-        </h2>
-        <p className={cx(`${BLOCK}__description`)}>
-          Before you delete your account,
-          <br />
-          please review the information below.
-        </p>
-      </div>
-      <SizedBox height='2rem' />
-      <ul className={cx(`${BLOCK}__list`)}>
-        {reviewList.map(({ icon: Icon, text }, index) => (
-          <li key={index} className={cx(`${BLOCK}__list-item`)}>
-            <Icon className={cx(`${BLOCK}__list-item__icon`)} />
-            <p className={cx(`${BLOCK}__list-item__text`)}>{text}</p>
-          </li>
-        ))}
-      </ul>
-      {/* TODO: 하단 위치고정도 생각하기 */}
-      <div className={cx(`${BLOCK}__buttons`)}>
-        <Button
-          color='white'
-          size='full'
-          className={cx('continue-button')}
-          onClick={handleContinueClick}
-        >
-          Continue using
-        </Button>
-        <Button color='green' size='full' onClick={handleDeleteClick}>
-          Delete Account
-        </Button>
+      <div className={cx('page')}>
+        <div className={cx('content')}>
+          <SizedBox height='2.75rem' />
+          <div className={cx(BLOCK)}>
+            <h2 className={cx(`${BLOCK}__title`)}>
+              Are you sure you want to
+              <br />
+              delete your Mandi account?
+            </h2>
+            <p className={cx(`${BLOCK}__description`)}>
+              Before you delete your account,
+              <br />
+              please review the information below.
+            </p>
+          </div>
+          <SizedBox height='2rem' />
+          <ul className={cx(`${BLOCK}__list`)}>
+            {reviewList.map(({ icon: Icon, text }, index) => (
+              <li key={index} className={cx(`${BLOCK}__list-item`)}>
+                <Icon className={cx(`${BLOCK}__list-item__icon`)} />
+                <p className={cx(`${BLOCK}__list-item__text`)}>{text}</p>
+              </li>
+            ))}
+          </ul>
+        </div>
+        <div className={cx(`bottom-area`)}>
+          <Button color='whitegray' size='full' onClick={handleContinueClick}>
+            Continue using
+          </Button>
+          <Button color='green' size='full' onClick={handleDeleteClick}>
+            Delete Account
+          </Button>
+        </div>
       </div>
       <Dialog
         isOpen={deleteDialogOpen}
@@ -90,17 +87,11 @@ export default function Page() {
         description='Are you sure you want to delete your account?'
         onClose={handleDialogClose}
         buttons={
-          <div
-            style={{
-              display: 'flex',
-              gap: '10px',
-            }}
-          >
-            {/* TODO: 버튼 색상 바꾸기 */}
+          <div className={cx(`dialog-buttons`)}>
             <Button size='full' color='darkgray' onClick={handleDialogClose}>
               Cancel
             </Button>
-            <Button size='full' color='green' onClick={deleteAccount}>
+            <Button size='full' color='red' onClick={deleteAccount}>
               Delete Account
             </Button>
           </div>
